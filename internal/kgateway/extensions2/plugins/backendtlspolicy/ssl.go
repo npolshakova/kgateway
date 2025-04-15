267ececfd73e0b0af160bdf0fba66c99e0dbad55
npolshakova <nina.polshakova@solo.io>
2025-04-15 13:47:34 -0400

    only ai backend ssl validation
    Signed-off-by: npolshakova <nina.polshakova@solo.io>

--- internal/kgateway/extensions2/plugins/backendtlspolicy/ssl.go
+++ internal/kgateway/extensions2/plugins/backendtlspolicy/ssl.go
@@ -1,14 +1,11 @@
 package backendtlspolicy
 
 import (
-	"crypto/x509"
-	"encoding/pem"
 	"errors"
-	"fmt"
-	"time"
 
 	envoycore "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	envoyauth "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
+	envoymatcher "github.com/envoyproxy/go-control-plane/envoy/type/matcher/v3"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -35,8 +32,11 @@ func ResolveCommonSslConfig(cm *corev1.ConfigMap, mustHaveCert bool) (*envoyauth
 		return nil, err
 	}
 
-	if err = validateCACertificate(caCrt); err != nil {
-		return nil, err
+	// TODO: should we do some validation on the CA?
+	caCrtData := envoycore.DataSource{
+		Specifier: &envoycore.DataSource_InlineString{
+			InlineString: caCrt,
+		},
 	}
 
 	tlsContext := &envoyauth.CommonTlsContext{
@@ -46,11 +46,7 @@ func ResolveCommonSslConfig(cm *corev1.ConfigMap, mustHaveCert bool) (*envoyauth
 
 	validationCtx := &envoyauth.CommonTlsContext_ValidationContext{
 		ValidationContext: &envoyauth.CertificateValidationContext{
-			TrustedCa: &envoycore.DataSource{
-				Specifier: &envoycore.DataSource_InlineString{
-					InlineString: caCrt,
-				},
-			},
+			TrustedCa: &caCrtData,
 		},
 	}
 	// sanList := VerifySanListToMatchSanList(cs.GetVerifySubjectAltName())
@@ -61,43 +57,6 @@ func ResolveCommonSslConfig(cm *corev1.ConfigMap, mustHaveCert bool) (*envoyauth
 	return tlsContext, err
 }
 
-func getValidationContext(cm *corev1.ConfigMap) (*envoyauth.CertificateValidationContext, error) {
-	caCrt, err := getSslSecrets(cm)
-	if err != nil {
-		return nil, err
-	}
-	if err = validateCACertificate(caCrt); err != nil {
-		return nil, err
-	}
-	return &envoyauth.CertificateValidationContext{
-		TrustedCa: &envoycore.DataSource{
-			Specifier: &envoycore.DataSource_InlineString{
-				InlineString: caCrt,
-			},
-		},
-	}, nil
-}
-
-func validateCACertificate(pemData string) error {
-	block, _ := pem.Decode([]byte(pemData))
-	if block == nil {
-		return errors.New("failed to decode PEM block")
-	}
-
-	cert, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
-		return fmt.Errorf("failed to parse certificate: %w", err)
-	}
-
-	if cert.NotAfter.Before(time.Now()) {
-		return errors.New("CA certificate has expired")
-	}
-
-	// TODO: add other validation (ex. IsCA?)
-
-	return nil
-}
-
 func getSslSecrets(cm *corev1.ConfigMap) (string, error) {
 	caCrt, ok := cm.Data["ca.crt"]
 	if !ok {
@@ -105,4 +64,15 @@ func getSslSecrets(cm *corev1.ConfigMap) (string, error) {
 	}
 
 	return caCrt, nil
+}
+
+func VerifySanListToMatchSanList(sanList []string) []*envoymatcher.StringMatcher {
+	var matchSanList []*envoymatcher.StringMatcher
+	for _, san := range sanList {
+		matchSan := &envoymatcher.StringMatcher{
+			MatchPattern: &envoymatcher.StringMatcher_Exact{Exact: san},
+		}
+		matchSanList = append(matchSanList, matchSan)
+	}
+	return matchSanList
 }
