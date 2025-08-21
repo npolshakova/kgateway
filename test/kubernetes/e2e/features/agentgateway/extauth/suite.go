5b90bfa21c791386a20982ca4341d03505350738
npolshakova <nina.polshakova@solo.io>
2025-08-20 23:11:57 -0400

    use old style test for agw
    Signed-off-by: npolshakova <nina.polshakova@solo.io>

--- test/kubernetes/e2e/features/agentgateway/extauth/suite.go
+++ test/kubernetes/e2e/features/agentgateway/extauth/suite.go
@@ -2,10 +2,13 @@ package extauth
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/onsi/gomega"
 	"github.com/stretchr/testify/suite"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/kubeutils"
@@ -13,73 +16,109 @@ import (
 	testmatchers "github.com/kgateway-dev/kgateway/v2/test/gomega/matchers"
 	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e"
 	testdefaults "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/defaults"
-	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/tests/base"
 )
 
 var _ e2e.NewSuiteFunc = NewTestingSuite
 
 // testingSuite is a suite of tests for ExtAuth functionality
 type testingSuite struct {
-	*base.BaseTestingSuite
+	suite.Suite
+
+	ctx context.Context
+
+	// testInstallation contains all the metadata/utilities necessary to execute a series of tests
+	// against an installation of kgateway
+	testInstallation *e2e.TestInstallation
+
+	// manifests shared by all tests
+	commonManifests []string
+	// resources from manifests shared by all tests
+	commonResources []client.Object
 }
 
 func NewTestingSuite(ctx context.Context, testInst *e2e.TestInstallation) suite.TestingSuite {
-	// Define the setup TestCase for common resources
-	setupTestCase := base.TestCase{
-		Manifests: []string{
-			testdefaults.CurlPodManifest,
-			simpleServiceManifest,
-			extAuthManifest,
-		},
-		Resources: []client.Object{
-			// resources from curl manifest
-			testdefaults.CurlPod,
-			// resources from service manifest
-			basicSecureRoute, simpleSvc, simpleDeployment,
-			// extauth resources
-			extAuthSvc, extAuthExtension,
-		},
-	}
-
-	// Define test-specific TestCases
-	testCases := map[string]base.TestCase{
-		"TestExtAuthPolicy": {
-			Manifests: []string{
-				gatewayWithRouteManifest,
-				securedGatewayPolicyManifest,
-				insecureRouteManifest,
-			},
-			Resources: []client.Object{
-				// deployer-generated resources
-				proxyDeployment, proxyService,
-				gatewayAttachedTrafficPolicy,
-				insecureRoute,
-			},
-		},
-		"TestRouteTargetedExtAuthPolicy": {
-			Manifests: []string{
-				gatewayWithRouteManifest,
-				securedRouteManifest,
-				insecureRouteManifest,
-			},
-			Resources: []client.Object{
-				// deployer-generated resources
-				proxyDeployment, proxyService,
-				secureRoute, secureTrafficPolicy,
-				insecureRoute, insecureTrafficPolicy,
-			},
-		},
-	}
-
 	return &testingSuite{
-		BaseTestingSuite: base.NewBaseTestingSuiteWithoutUpgrades(ctx, testInst, setupTestCase, testCases),
+		ctx:              ctx,
+		testInstallation: testInst,
 	}
+}
+
+func (s *testingSuite) SetupSuite() {
+	s.commonManifests = []string{
+		testdefaults.CurlPodManifest,
+		simpleServiceManifest,
+		extAuthManifest,
+	}
+	s.commonResources = []client.Object{
+		// resources from curl manifest
+		testdefaults.CurlPod,
+		// resources from service manifest
+		basicSecureRoute, simpleSvc, simpleDeployment,
+		// extauth resources
+		extAuthSvc, extAuthExtension,
+	}
+
+	// set up common resources once
+	for _, manifest := range s.commonManifests {
+		err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, manifest)
+		s.Require().NoError(err, "can apply "+manifest)
+	}
+	s.testInstallation.Assertions.EventuallyObjectsExist(s.ctx, s.commonResources...)
+
+	// make sure pods are running
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.GetNamespace(), metav1.ListOptions{
+		LabelSelector: testdefaults.CurlPodLabelSelector,
+	})
+
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, proxyObjMeta.GetNamespace(), metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("app.kubernetes.io/name=%s", proxyObjMeta.GetName()),
+	}, time.Minute*2)
+}
+
+func (s *testingSuite) TearDownSuite() {
+	// clean up common resources
+	for _, manifest := range s.commonManifests {
+		err := s.testInstallation.Actions.Kubectl().DeleteFileSafe(s.ctx, manifest)
+		s.Require().NoError(err, "can delete "+manifest)
+	}
+	s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, s.commonResources...)
+
+	s.testInstallation.Assertions.EventuallyPodsNotExist(s.ctx, proxyObjMeta.GetNamespace(), metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("app.kubernetes.io/name=%s", proxyObjMeta.GetName()),
+	})
 }
 
 // TestExtAuthPolicy tests the basic ExtAuth functionality with header-based allow/deny
 // Checks for gateway level auth with route level opt out
 func (s *testingSuite) TestExtAuthPolicy() {
-	// The BaseTestingSuite automatically handles setup and cleanup of test-specific resources
+	manifests := []string{
+		gatewayWithRouteManifest,
+		securedGatewayPolicyManifest,
+		insecureRouteManifest,
+	}
+
+	resources := []client.Object{
+		gatewayAttachedTrafficPolicy,
+		insecureRoute,
+		// deployer-generated resources
+		proxyDeployment, proxyService,
+	}
+	s.T().Cleanup(func() {
+		for _, manifest := range manifests {
+			err := s.testInstallation.Actions.Kubectl().DeleteFileSafe(s.ctx, manifest)
+			s.Require().NoError(err)
+		}
+		s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, resources...)
+	})
+	// set up common resources once
+	for _, manifest := range manifests {
+		err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, manifest)
+		s.Require().NoError(err, "can apply "+manifest)
+	}
+	s.testInstallation.Assertions.EventuallyObjectsExist(s.ctx, resources...)
+
+	// Wait for pods to be running
+	s.ensureBasicRunning()
 
 	testCases := []struct {
 		name                         string
@@ -134,8 +173,8 @@ func (s *testingSuite) TestExtAuthPolicy() {
 			}
 
 			// Test the request
-			s.TestInstallation.Assertions.AssertEventualCurlResponse(
-				s.Ctx,
+			s.testInstallation.Assertions.AssertEventualCurlResponse(
+				s.ctx,
 				testdefaults.CurlPodExecOpt,
 				opts,
 				&testmatchers.HttpResponse{
@@ -148,7 +187,34 @@ func (s *testingSuite) TestExtAuthPolicy() {
 
 // TestRouteTargetedExtAuthPolicy tests route level only extauth
 func (s *testingSuite) TestRouteTargetedExtAuthPolicy() {
-	// The BaseTestingSuite automatically handles setup and cleanup of test-specific resources
+	manifests := []string{
+		gatewayWithRouteManifest,
+		securedRouteManifest,
+		insecureRouteManifest,
+	}
+
+	resources := []client.Object{
+		secureRoute, secureTrafficPolicy,
+		insecureRoute, insecureTrafficPolicy,
+		// deployer-generated resources
+		proxyDeployment, proxyService,
+	}
+	s.T().Cleanup(func() {
+		for _, manifest := range manifests {
+			err := s.testInstallation.Actions.Kubectl().DeleteFileSafe(s.ctx, manifest)
+			s.Require().NoError(err)
+		}
+		s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, resources...)
+	})
+	// set up common resources once
+	for _, manifest := range manifests {
+		err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, manifest)
+		s.Require().NoError(err, "can apply "+manifest)
+	}
+	s.testInstallation.Assertions.EventuallyObjectsExist(s.ctx, resources...)
+
+	// Wait for pods to be running
+	s.ensureBasicRunning()
 
 	testCases := []struct {
 		name                         string
@@ -201,8 +267,8 @@ func (s *testingSuite) TestRouteTargetedExtAuthPolicy() {
 			}
 
 			// Test the request
-			s.TestInstallation.Assertions.AssertEventualCurlResponse(
-				s.Ctx,
+			s.testInstallation.Assertions.AssertEventualCurlResponse(
+				s.ctx,
 				testdefaults.CurlPodExecOpt,
 				opts,
 				&testmatchers.HttpResponse{
@@ -211,4 +277,16 @@ func (s *testingSuite) TestRouteTargetedExtAuthPolicy() {
 				})
 		})
 	}
+}
+
+func (s *testingSuite) ensureBasicRunning() {
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.GetNamespace(), metav1.ListOptions{
+		LabelSelector: "app.kubernetes.io/name=curl",
+	})
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, proxyObjMeta.GetNamespace(), metav1.ListOptions{
+		LabelSelector: "app.kubernetes.io/name=super-gateway",
+	}, time.Minute)
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, extAuthSvc.GetNamespace(), metav1.ListOptions{
+		LabelSelector: "app=ext-authz",
+	})
 }
