740df37b8a20d74c00fed0acdc230f21e58b24c2
npolshakova <nina.polshakova@solo.io>
2026-01-27 10:43:10 -0500

    more fixes for codespace
    Signed-off-by: npolshakova <nina.polshakova@solo.io>

--- tools/build-tools/prow-entrypoint.sh
+++ tools/build-tools/prow-entrypoint.sh
@@ -1,19 +1,4 @@
-#!/usr/bin/env bash
-
-# Copyright Istio Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# Based on Istio's build-tools image: https://github.com/istio/tools/tree/228d21452fd640bf7389d7d41fecaa715ce73249/docker/build-tools 
 set -euo pipefail
 
 # Minimal entrypoint for CI environments that want a running dockerd inside the container.
